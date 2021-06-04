24687cf68a38945d053ea0e3ac649e2fdeda1c2d
Brian Dowd <dowdbrian@yahoo.com>
2021-06-04 13:29:13 +0200

    done

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      {/*<header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -17,7 +17,46 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </header>*/}
+      <section className="App-section1">  
+        <nav>
+          <ul className="App-nav">
+            <li>
+              <img src="/images/ironhack-logo.svg" />
+            </li>
+            <li>
+              <img src="/images/menu-top.svg" />
+            </li>
+          </ul>
+        </nav>
+        <header className="App-header">
+          <h1>Say hello to ReactJS</h1>
+          <p>You will learn how to use the most popular frontend library, and become a super Ninja developer.</p>
+          <a href="#">Awesome!</a>
+        </header>
+      </section>
+      <section className="App-section2">
+        <div>
+          <img src="images/icon1.png"/>
+          <h2>Declarative</h2>
+          <p>React makes it painless to create interactive UIs.</p>
+        </div>
+        <div>
+          <img src="images/icon2.png"/>
+          <h2>Components</h2>
+          <p>Built encapuslated components that manage their state.</p>
+        </div>
+        <div>
+          <img src="images/icon3.png"/>
+          <h2>Single-Way</h2>
+          <p>A set of immutable values are passed to the components.</p>
+        </div>
+        <div>
+          <img src="images/icon4.png"/>
+          <h2>JSX</h2>
+          <p>Statically-typed. designed to run on modern browsers.</p>
+        </div>
+      </section>
     </div>
   );
 }
